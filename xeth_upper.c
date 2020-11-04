platinasystems/linux

xeth: disallow linux vlan and bridge
An xeth port may only be a member of an xeth bridge or lag;
and may only contain xeth vlans, not linux vlans.

Signed-off-by: Tom Grennan <tgrennan@platinasystems.com>

drivers/net/ethernet/xeth/xeth_upper.c
@@ -739,14 +739,8 @@ static void xeth_upper_lnko_setup_port(struct net_device *nd)
 	nd->priv_flags |= IFF_LIVE_ADDR_CHANGE;
 	nd->priv_flags |= IFF_NO_QUEUE;
 	nd->priv_flags |= IFF_PHONY_HEADROOM;
-#ifdef __FIXME__
-	nd->features |= NETIF_F_LLTX;
-	nd->features |= VETH_FEATURES;
-
-	nd->hw_features = VETH_FEATURES;
-	nd->hw_enc_features = VETH_FEATURES;
-	nd->mpls_features = NETIF_F_HW_CSUM | NETIF_F_GSO_SOFTWARE;
-#endif /* __FIXEME__ */
+	nd->priv_flags |= IFF_DONT_BRIDGE;
+	nd->features |= NETIF_F_VLAN_CHALLENGED;
 	nd->min_mtu = xeth_mux->min_mtu;
 	nd->max_mtu = xeth_mux->max_mtu;
 }
@@ -761,14 +755,8 @@ static void xeth_upper_lnko_setup_vlan(struct net_device *nd)
 	nd->priv_flags |= IFF_LIVE_ADDR_CHANGE;
 	nd->priv_flags |= IFF_NO_QUEUE;
 	nd->priv_flags |= IFF_PHONY_HEADROOM;
-#ifdef __FIXME__
-	nd->features |= NETIF_F_LLTX;
-	nd->features |= VETH_FEATURES;
-
-	nd->hw_features = VETH_FEATURES;
-	nd->hw_enc_features = VETH_FEATURES;
-	nd->mpls_features = NETIF_F_HW_CSUM | NETIF_F_GSO_SOFTWARE;
-#endif /* __FIXEME__ */
+	nd->priv_flags |= IFF_DONT_BRIDGE;
+	nd->features |= NETIF_F_VLAN_CHALLENGED;
 }
 
 static void xeth_upper_lnko_setup_bridge_or_lag(struct net_device *nd)
@@ -782,15 +770,9 @@ static void xeth_upper_lnko_setup_bridge_or_lag(struct net_device *nd)
 	nd->priv_flags |= IFF_LIVE_ADDR_CHANGE;
 	nd->priv_flags |= IFF_NO_QUEUE;
 	nd->priv_flags |= IFF_PHONY_HEADROOM;
+	nd->priv_flags |= IFF_DONT_BRIDGE;
+	nd->features |= NETIF_F_VLAN_CHALLENGED;
 	nd->features |= NETIF_F_NETNS_LOCAL;
-#ifdef __FIXME__
-	nd->features |= NETIF_F_LLTX;
-	nd->features |= VETH_FEATURES;
-
-	nd->hw_features = VETH_FEATURES;
-	nd->hw_enc_features = VETH_FEATURES;
-	nd->mpls_features = NETIF_F_HW_CSUM | NETIF_F_GSO_SOFTWARE;
-#endif /* __FIXEME__ */
 }
 
 static int xeth_upper_lnko_validate_vlan(struct nlattr *tb[],

platinasystems/xeth

xeth_upper.c
@@ -669,6 +669,8 @@ static void xeth_upper_lnko_setup_port(struct net_device *upper)
 	upper->priv_flags |= IFF_LIVE_ADDR_CHANGE;
 	upper->priv_flags |= IFF_NO_QUEUE;
 	upper->priv_flags |= IFF_PHONY_HEADROOM;
+	upper->priv_flags |= IFF_DONT_BRIDGE;
+	upper->features |= NETIF_F_VLAN_CHALLENGED;
 }
 
 static void xeth_upper_lnko_setup_vlan(struct net_device *upper)
@@ -681,6 +683,8 @@ static void xeth_upper_lnko_setup_vlan(struct net_device *upper)
 	upper->priv_flags |= IFF_LIVE_ADDR_CHANGE;
 	upper->priv_flags |= IFF_NO_QUEUE;
 	upper->priv_flags |= IFF_PHONY_HEADROOM;
+	upper->priv_flags |= IFF_DONT_BRIDGE;
+	upper->features |= NETIF_F_VLAN_CHALLENGED;
 }
 
 static void xeth_upper_lnko_setup_bridge_or_lag(struct net_device *upper)
@@ -694,6 +698,8 @@ static void xeth_upper_lnko_setup_bridge_or_lag(struct net_device *upper)
 	upper->priv_flags |= IFF_LIVE_ADDR_CHANGE;
 	upper->priv_flags |= IFF_NO_QUEUE;
 	upper->priv_flags |= IFF_PHONY_HEADROOM;
+	upper->priv_flags |= IFF_DONT_BRIDGE;
+	upper->features |= NETIF_F_VLAN_CHALLENGED;
 	upper->features |= NETIF_F_NETNS_LOCAL;
 }
 